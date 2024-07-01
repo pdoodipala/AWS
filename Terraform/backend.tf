8b1f580f3f53a5ab6303c1fc15f4c9fc2c15a15c
pdoodipala <pdoodipala2@gmail.com>
2024-07-01 22:24:12 +0530

    updated

---
+++ Terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-s3-tf" # change this
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
